c5543b32a4667fd5f4227bd9f16e7f575e9ea0b7
Jaime <jtbenavente@gmail.com>
2013-10-18 10:42:04 +0200

    añadido enlace a origen d ela img de fondo

--- php/index.php
+++ php/index.php
@@ -62,8 +62,91 @@
 				<li><input type="submit" id="enviar" value="Generar archivo"/></li>
 			</ul>
 		</form>
-
 		</section>
 		<p>Imagen obtenida de <a href="http://nacidoparacuriosear.blogspot.com.es/2012/10/almacenamiento-en-cristales-datos-para.html">este blog</a>.</p>
+	    <?php
+			$dsn = "mysql:host=127.3.193.130;port:3306;dbname=generadordeobjetivos";
+			$usuario= "adminRX1aPbt";
+			$clave= "2L5BkMa4NaiS";
+			if(isset($_POST['ejercicios']) && !empty($_POST['ejercicios'])){
+
+				//En primer lugar creamos una copia de los objetivos sin marcar
+				$objetivos="files/todosObjetivos.md";
+				$destino="files/objetivos.md";
+
+				if(!copy($objetivos, $destino)){
+  				die("No se ha podido copiar el archivo.");
+				}
+
+				//Abrimos el archivo
+				$file = fopen($destino,"r+") or exit("No se ha podido abrir el archivo");
+
+				//Abrimos la conexion
+				try {
+					$conexion = new PDO( $dsn, $usuario, $clave );
+					$conexion->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+				}catch ( PDOException $e ) { echo "Conexión fallida: " . $e->getMessage();	}
+
+				//Recorremos todos los ejercicios marcados y vamos memoriando los objetivos ya cumplidos
+				foreach ($_POST['ejercicios'] as $ejercicio) {
+			
+					//obtenemos los objetivos que se cumplen consultando la base de datos
+					$consultaSQL = 'SELECT * FROM  `generadordeobjetivos`.`ejercicios` WHERE  `ejercicio` ='.$ejercicio;
+					
+					//ejecutamos la consulta
+					try { 
+						$resultados=$conexion->query( $consultaSQL ); 
+					}catch ( PDOException $e ) { echo "Consulta fallida: " . $e->getMessage(); }
+						
+						$datos=$resultados->fetchAll();
+						//Guardamos el valor de los objetivos
+						$x=$datos[0]['objetivo'];
+						$x2=$datos[0]['objetivo2'];
+
+						//Establecemos el puntero de lectura/escritura al principio del fichero
+						rewind($file);
+
+						$contador=0;
+						$fin=false;
+						//Buscamos el objetivo1 en el fichero y lo marcamos como cumplido
+						while ((false !== ($caracter = fgetc($file))) and false == $fin) {
+    					if ($caracter=='[') {
+    						$contador+=1;
+    						if ($contador==$x) {
+    							fwrite($file,'x]');
+    							$fin=true;
+    						}
+    					}
+						}
+
+						//Comprobamos si hay otro objetivo que se cumpla con este ejercicio
+						if (condition) {
+							//Establecemos el puntero de lectura/escritura al principio del fichero
+							rewind($file);
+
+							$contador=0;
+							$fin=false;
+							//Buscamos el objetivo2 en el fichero y lo marcamos como cumplido
+							while ((false !== ($caracter = fgetc($file))) and false == $fin) {
+	    					if ($caracter=='[') {
+	    						$contador+=1;
+	    						if ($contador==$x2) {
+	    							fwrite($file,'x]');
+	    							$fin=true;
+	    						}
+	    					}
+							}
+						}
+				}
+
+				
+
+				$conexion=NULL;
+				fclose($file);
+
+				//enviamos el archivo
+				header('Location:files/descargar.php');
+			}
+		?>
 	</body>
 </html>
